a86a09f6011db282425af2182a36e33560b9b046
Awesome Stickz <kiran121102@gmail.com>
2021-05-22 17:23:39 +0530

    Fixed a bug where links from certain domains would never work, by using "got" package instead of "node-fetch"

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,22 @@
 import { JSDOM } from 'jsdom';
-import AbortController from 'abort-controller';
-import fetch from 'node-fetch';
 import isURL from 'is-url';
 import getImageType from 'image-type';
+import got from 'got';
 
 export const verifyImageURL = async (url: string, options?: { timeout: number }) => {
     const getReturnValue = (isImage = false, imageURL = url) => ({ isImage, imageURL });
     if (!isURL(url)) return getReturnValue();
 
-    const abortController = new AbortController();
-    const timeout = setTimeout(() => abortController.abort(), options?.timeout ?? 5000);
-
     try {
-        const response = await fetch(url, { signal: abortController.signal });
-        const buffer = Buffer.from(await response.arrayBuffer());
-        const imageType = getImageType(buffer);
-
-        clearTimeout(timeout);
+        const responseBuffer = (await got(url, { timeout: options?.timeout ?? 5000 })).rawBody;
+        const imageType = getImageType(responseBuffer);
 
         if (!imageType?.mime.startsWith('image')) {
-            const responseText = await (await fetch(url, { signal: abortController.signal })).text();
-
-            if (responseText.includes('og:image')) {
-                const dom = new JSDOM(responseText);
+            if (responseBuffer.includes('og:image')) {
+                const dom = new JSDOM(responseBuffer);
                 const meta = dom.window.document.querySelector('meta[property="og:image"]');
 
-                if (!isURL(meta.content)) return getReturnValue();
+                if (!meta || !meta.content || !isURL(meta.content)) return getReturnValue();
 
                 if (!/^https?:/.test(meta.content)) {
                     if (/^\/\//.test(meta.content)) meta.content = `http:${meta.content}`;
@@ -37,9 +28,7 @@ export const verifyImageURL = async (url: string, options?: { timeout: number })
             }
         } else return getReturnValue(true);
     } catch (err) {
-        if (err.name !== 'AbortError') console.error(err);
-
-        clearTimeout(timeout);
+        if (err.code !== 'ETIMEDOUT') console.error(err);
     }
 
     return getReturnValue();
